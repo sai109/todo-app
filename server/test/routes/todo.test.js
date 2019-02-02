1a6573b98e09be46eac11f9006c202f6291d1406
sai109 <lukebraithwaite7@gmail.com>
2019-02-02 12:09:20 +0000

    test files refactored

--- server/test/routes/todo.test.js
+++ server/test/routes/todo.test.js
@@ -1,15 +1,15 @@
 const expect = require('expect');
-const { app } = require('../server');
+const { app } = require('../../server');
 const request = require('supertest');
-const { User } = require('../models/user');
+const { User } = require('../../models/user');
 const {
 	users,
 	todos,
 	populateUsers,
 	populateTodos,
 	todoOneID
-} = require('./seeds/seeds');
-const { Todo } = require('../models/todo');
+} = require('../seeds/seeds');
+const { Todo } = require('../../models/todo');
 const jwt = require('jsonwebtoken');
 const { ObjectID } = require('mongodb');
 
@@ -22,120 +22,6 @@ afterEach(done => {
 
 afterEach(done => {
 	User.remove({}).then(() => done());
-});
-
-describe('POST /register', () => {
-	it('should register a new user', done => {
-		const email = 'example@gmail.com';
-		const password = '1234567';
-		request(app)
-			.post('/api/register')
-			.send({ email, password })
-			.expect(200)
-			.end(err => {
-				if (err) {
-					return done(err);
-				}
-				User.findOne({ email })
-					.then(user => {
-						expect(user.email).toBe(email);
-						expect(user.password).not.toBe(password);
-						return done();
-					})
-					.catch(err => done(err));
-			});
-	});
-
-	it('should send 400 if email already exists', done => {
-		const email = users[0].email;
-		const password = '1234568';
-		request(app)
-			.post('/api/register')
-			.send({ email, password })
-			.expect(400)
-			.end(done);
-	});
-
-	it('should send 400 if email not provided', done => {
-		const password = '12345678';
-		request(app)
-			.post('/api/register')
-			.send({ password })
-			.expect(400)
-			.end(done);
-	});
-
-	it('should send 400 if email not valid', done => {
-		const password = '12345678';
-		const email = 'luke123.com';
-		request(app)
-			.post('/api/register')
-			.send({ email, password })
-			.expect(400)
-			.end(done);
-	});
-
-	it('should send 400 if password not provided', done => {
-		const email = 'example@gmail.com';
-		request(app)
-			.post('/api/register')
-			.send({ email })
-			.expect(400)
-			.end(done);
-	});
-
-	it('should send 400 if password less than 6 characters', done => {
-		const email = 'example@gmail.com';
-		const password = '1234';
-		request(app)
-			.post('/api/register')
-			.send({ email, password })
-			.expect(400)
-			.end(done);
-	});
-});
-
-describe('GET /login', () => {
-	const email = 'example@example.com';
-	const password = '1234567';
-	it('should logine if email and password are correct', done => {
-		request(app)
-			.post('/api/register')
-			.send({ email, password })
-			.expect(200)
-			.end(() => {
-				request(app)
-					.get('/api/login')
-					.send({ email, password })
-					.expect(200)
-					.end(done);
-			});
-	});
-
-	it('should send 400 if email not provided', done => {
-		request(app)
-			.post('/api/register')
-			.send({ password })
-			.expect(400)
-			.end(done);
-	});
-
-	it('should send 400 if email not valid', done => {
-		const email = 'luke123.com';
-		request(app)
-			.post('/api/register')
-			.send({ email, password })
-			.expect(400)
-			.end(done);
-	});
-
-	it('should send 400 if password not provided', done => {
-		request(app)
-			.post('/api/register')
-			.send({ email })
-			.expect(400)
-			.end(done);
-	});
 });
 
 describe('GET /todos', () => {
