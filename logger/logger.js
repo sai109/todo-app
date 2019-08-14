4baf1ab2cc3169aa16a039bf9e8c507a6efcaa49
AspieCoder <lukebraithwaite7@gmail.com>
2019-08-14 18:03:01 +0100

    Logger extra characters fixed

--- logger/logger.js
+++ logger/logger.js
@@ -1,29 +1,36 @@
-var winston = require('winston');
 const path = require('path');
+const { createLogger, format, transports } = require('winston');
 
-// define the custom settings for each transport (file, console)
+const { combine, timestamp, label, printf, splat, json } = format;
+
 var options = {
 	file: {
-		level: 'error',
+		level: 'info',
 		filename: path.join(__dirname, '../logs/server.log'),
 		handleExceptions: true,
-		json: true,
 		maxsize: 5242880, // 5MB
 		maxFiles: 5,
-		colorize: true,
 	},
 };
 
-// instantiate a new Winston Logger with the settings defined above
-var logger = winston.createLogger({
-	transports: [new winston.transports.File(options.file)],
-	exitOnError: false, // do not exit on handled exceptions
+const myFormat = printf(info => {
+	return `${info.timestamp} ${info.label} ${info.level}: ${info.message}`;
+});
+
+const logger = createLogger({
+	format: combine(
+		label({ label: '[logger]' }),
+		timestamp(),
+		json(),
+		splat(),
+		myFormat,
+	),
+	transports: [new transports.File(options.file)],
 });
 
 // create a stream object with a 'write' function that will be used by `morgan`
 logger.stream = {
 	write: function(message) {
-		// use the 'info' log level so the output will be picked up by both transports (file and console)
 		logger.info(message);
 	},
 };
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,6 +6,6 @@ mongoose.Promise = global.Promise;
 mongoose
 	.connect(process.env.DB_URL, { useNewUrlParser: true })
 	.then()
-	.catch(e => logger.error(`Unable to connect to MongDB: ${e}`));
+	.catch(() => logger.error('Unable to connect to MongDB'));
 
 module.exports = { mongoose };
