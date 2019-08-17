d16e6f905dc7f2f890bdc03c90f9408ae406719d
AspieCoder <lukebraithwaite7@gmail.com>
2019-08-17 21:48:24 +0100

    Auth action tests added

--- src/tests/redux/actions/auth.test.js
+++ src/tests/redux/actions/auth.test.js
@@ -1,16 +1,12 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import fetchMock from 'fetch-mock';
+
 import * as userActions from '../../../redux/actions/auth';
 import mockAxios from 'axios';
 import history from 'history';
 
 const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
-
-afterEach(() => {
-	fetchMock.reset();
-});
 
 it('should login a user', done => {
 	mockAxios.post.mockImplementationOnce(() =>
@@ -48,5 +44,117 @@ it('should login a user', done => {
 		expect(history.push).toHaveBeenCalledWith('/dashboard');
 	});
 
+	done();
+});
+
+it('should not login a user', done => {
+	mockAxios.post.mockImplementationOnce(() =>
+		Promise.reject({
+			payload: {
+				email: 'That email is already taken',
+			},
+		}),
+	);
+
+	const expectedActions = [
+		{
+			type: 'GET_ERRORS',
+			payload: {
+				email: 'That email is already taken',
+			},
+		},
+	];
+
+	const userData = {
+		email: 'test@example.com',
+		password: 'A123456*&a',
+	};
+	const store = mockStore();
+
+	store.dispatch(userActions.loginUser(userData, history)).then(() => {
+		expect(store.getActions()).toEqual(expectedActions);
+	});
+
+	done();
+});
+
+it('should register a user', done => {
+	mockAxios.post.mockImplementationOnce(() =>
+		Promise.resolve({
+			id: 'Test',
+			token: 'RANDOM_JWT_TOKEN',
+		}),
+	);
+
+	const userData = {
+		email: 'test@test.io',
+		password: '123454rA%&',
+	};
+
+	const expectedActions = [
+		{
+			type: 'CLEAR_ERRORS',
+		},
+	];
+
+	const store = mockStore();
+
+	store.dispatch(userActions.registerUser(userData)).then(() => {
+		expect(store.getActions()).toEqual(expectedActions);
+	});
+	done();
+});
+
+it('should not register a user', done => {
+	mockAxios.post.mockImplementationOnce(() =>
+		Promise.reject({
+			payload: {
+				email: 'That email is already taken',
+			},
+		}),
+	);
+
+	const expectedActions = [
+		{
+			type: 'GET_ERRORS',
+			payload: {
+				email: 'That email is already taken',
+			},
+		},
+	];
+
+	const userData = {
+		email: 'test@example.com',
+		password: 'A123456*&a',
+	};
+	const store = mockStore();
+
+	store.dispatch(userActions.loginUser(userData, history)).then(() => {
+		expect(store.getActions()).toEqual(expectedActions);
+	});
+
+	done();
+});
+
+it('should logout a user', done => {
+	const store = mockStore();
+	store.dispatch(userActions.logoutUser());
+	expect(store.getActions()).toEqual([{ type: 'LOGOUT' }]);
+	done();
+});
+
+it('should set current user', done => {
+	const store = mockStore();
+
+	store.dispatch(userActions.setCurrentUser({ id: 'TEST_ID' }, 'TEST_TOKEN'));
+	expect(store.getActions()).toEqual([
+		{
+			type: 'SET_USER',
+			payload: {
+				id: 'TEST_ID',
+				token: 'TEST_TOKEN',
+			},
+		},
+	]);
 	done();
 });
