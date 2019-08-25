371e5ad1c08bdd85a321484522751520d3098d32
AspieCoder <lukebraithwaite7@gmail.com>
2019-08-25 17:21:25 +0100

    Error reducer tests added

---
+++ src/tests/redux/reducers/error.test.js
@@ -0,0 +1,23 @@
+import errorReducer from '../../../redux/reducer/errors';
+
+it('should handle get errors', () => {
+	const action = {
+		type: 'GET_ERRORS',
+		payload: {
+			email: 'email already exists',
+		},
+	};
+
+	const state = errorReducer({}, action);
+	expect(state).toHaveProperty('email', 'email already exists');
+});
+
+it('should handle clear error', () => {
+	const action = {
+		type: 'CLEAR_ERRORS',
+	};
+
+	const state = errorReducer({ email: 'email already exists' }, action);
+
+	expect(state).toEqual({});
+});
