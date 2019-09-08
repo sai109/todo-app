4a2380ab6a9ce9ac5882b02317b5c6a61ce38af1
AspieCoder <lukebraithwaite7@gmail.com>
2019-09-08 13:09:19 +0100

    routes refactored

--- backend-ts/src/routes/todo.ts
+++ backend-ts/src/routes/todo.ts
@@ -1,17 +1,28 @@
-const express = require('express');
-const passport = require('passport');
+import * as express from 'express';
+import * as passport from 'passport';
 const { Todo } = require('../models/todo');
-const { ObjectID } = require('mongodb');
+import { ObjectID } from 'mongodb';
 const logger = require('../logger/logger');
 const _ = require('lodash');
 
-const router = express.Router();
+interface ITodoRequest extends express.Request {
+	user: {
+		_id: ObjectID;
+	};
+}
+
+interface Iupdates {
+	body?: string;
+	completed?: boolean;
+}
+
+const router: express.Router = express.Router();
 
 // POST /todo - Adds a new todo
 router.post(
 	'/todo',
 	passport.authenticate('jwt', { session: false }),
-	(req, res) => {
+	(req: ITodoRequest, res: express.Response) => {
 		if (!req.body.todo) {
 			return res.status(400).send({ noTodo: 'Please provide a todo to add' });
 		}
@@ -25,14 +36,14 @@ router.post(
 			.save()
 			.then(() => res.status(200).json({ newTodo }))
 			.catch(err => logger.error(err));
-	}
+	},
 );
 
 // GET /todos - Gets all todos associated with user
 router.get(
 	'/todos',
 	passport.authenticate('jwt', { session: false }),
-	(req, res) => {
+	(req: ITodoRequest, res: express.Response) => {
 		Todo.find({ _creator: req.user._id })
 			.then(todos => {
 				if (todos.length > 0) {
@@ -42,14 +53,14 @@ router.get(
 				}
 			})
 			.catch(err => logger.error(err));
-	}
+	},
 );
 
 // GET /todo/:id - Gets an individual todo
 router.get(
 	'/todo/:id',
 	passport.authenticate('jwt', { session: false }),
-	(req, res) => {
+	(req: ITodoRequest, res) => {
 		const id = req.params.id;
 		const creator = req.user._id;
 		if (ObjectID.isValid(id)) {
@@ -67,16 +78,16 @@ router.get(
 				error: 'Please a valid id for the todo you would like to find',
 			});
 		}
-	}
+	},
 );
 
 // PATCH /todo/:id - Edits an individual todo
 router.patch(
 	'/todo/:id',
 	passport.authenticate('jwt', { session: false }),
-	(req, res) => {
+	(req: ITodoRequest, res: express.Response) => {
 		const id = req.params.id;
-		const updates = {};
+		const updates: Iupdates = {};
 		const { body, completed } = req.body;
 
 		if (body) {
@@ -95,7 +106,7 @@ router.patch(
 			{
 				$set: updates,
 			},
-			{ new: true }
+			{ new: true },
 		)
 			.then(todo => {
 				if (todo) {
@@ -105,14 +116,14 @@ router.patch(
 				}
 			})
 			.catch(err => logger.error(err));
-	}
+	},
 );
 
 // DELETE /todo/:id - Should delete a todo
 router.delete(
 	'/todo/:id',
 	passport.authenticate('jwt', { session: false }),
-	(req, res) => {
+	(req: ITodoRequest, res: express.Response) => {
 		const id = req.params.id;
 		if (!ObjectID.isValid(id)) {
 			return res.status(400).send({ error: 'Please submit a valid ID' });
@@ -126,6 +137,6 @@ router.delete(
 				}
 			})
 			.catch(err => res.status(404).send(err));
-	}
+	},
 );
 module.exports = router;
