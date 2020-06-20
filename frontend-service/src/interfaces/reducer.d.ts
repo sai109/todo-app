88725486f49935c5ea579e39e484f5097611a3df
AspieCoder <lukebraithwaite7@gmail.com>
2020-06-20 17:08:30 +0100

    Types for Auth Reducer added

--- frontend-service/src/interfaces/reducer.d.ts
+++ frontend-service/src/interfaces/reducer.d.ts
@@ -1,5 +1,12 @@
+import { IUser } from './user';
+
 interface ITodoReducer {
 	todos: ITodo[];
 	todo: ITodo | {};
 	loading: boolean;
+}
+
+interface IAuthReducer {
+	token?: String;
+	id?: IUser['_id'];
 }
